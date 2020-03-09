3e1dac540832ee7fdd2d34bc9f50327a0dcc3ade
PalakPartani <partanipalak16@gmail.com>
2020-03-09 11:06:59 +0530

    ADDED:code and test case to find top batting average

--- src/test/java/com/iplanalyser/IplAnalyserTest.java
+++ src/test/java/com/iplanalyser/IplAnalyserTest.java
@@ -8,6 +8,22 @@ import org.junit.Test;
 public class IplAnalyserTest {
     private static IplAnalyser iplAnalyser;
     private static final String IPL_MOST_RUNS_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        iplAnalyser = new IplAnalyser();
+    }
+
+    @Test
+    public void givenCricketMostRunData_WhenSorted_ShouldReturnMostRuns() {
+        try {
+            iplAnalyser.loadIplData(IPL_MOST_RUNS_FILE_PATH);
+            String sortedCricketData = iplAnalyser.getSortedCricketData(SortField.AVG);
+            IPLDTOClass[] mostRunCsv = new Gson().fromJson(sortedCricketData, IPLDTOClass[].class);
+            Assert.assertEquals(83.2, mostRunCsv[0].battingAvg, 0.0);
+        } catch (IplAnalyserException e) {
+        }
+    }
 }
 
 
